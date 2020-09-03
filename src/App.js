89475771ebab47313176665f0e6882f61fa4d5bb
Nour Balaha <nourbalaha0@gmail.com>
2020-09-03 22:17:01 +0900

    added slideshow component to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './App.scss'
 
 import Navbar from './components/Navbar/Navbar.component'
+import Slideshow from './components/Slideshow/Slideshow.component'
 
 function App () {
   return (
     <div className='app'>
       <header className='app-header'>
         <Navbar />
-      </header>
+        </header>
+        <Slideshow />
     </div>
   )
 }
