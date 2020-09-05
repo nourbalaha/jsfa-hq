bf329c1227359ac99dedb9121d3007eed45fb65b
Nour Balaha <nourbalaha0@gmail.com>
2020-09-06 01:20:36 +0900

    added other components to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.scss'
 
 import Navbar from './components/Navbar/Navbar.component'
 import Slideshow from './components/Slideshow/Slideshow.component'
+import Page from './components/Page/Page.component'
+import Footer from './components/Footer/Footer.component'
 
 function App () {
   return (
@@ -11,6 +13,12 @@ function App () {
         <Navbar />
         </header>
         <Slideshow />
+        <Page title="About" id="about" />
+        <Page title="Committee" id="committee" />
+        <Page title="Education Center" id="education" />
+        <Page title="Activities" id="activities" />
+        <Page title="Information" id="information" />
+        <Footer />
     </div>
   )
 }
