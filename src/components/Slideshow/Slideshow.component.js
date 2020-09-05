45079287cf6ecbaa14f3cb5360a986a09dee8b60
Nour Balaha <nourbalaha0@gmail.com>
2020-09-06 01:18:59 +0900

    removed moving photos from slideshow

--- src/components/Slideshow/Slideshow.component.js
+++ src/components/Slideshow/Slideshow.component.js
@@ -1,49 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Slideshow.style.scss'
 
+import logo from "../../assets/JSFA logo.png"
+
 export default function Slideshow() {
-    let [index, setIndex] = useState(0);
-
-    useEffect(()=>{
-        setTimeout(()=>{
-                index<2?setIndex(index+1):setIndex(0);
-        },5000)
-    },[index])
-
     return (
         <div>
-        {
-            index===0
-            ?
-                <div class="slideshow-container">
-                    <div class="mySlides show image1">
-                        <span className="text1">Japan Syria Friendship Association</span>
-                        <span className="text2">Non-Governmental Organization</span>
-                    </div>
-                    <div class="mySlides hide" />
-                    <div class="mySlides hide" />
+            <div class="slideshow-container">
+                <div class="mySlides show image">
+                    <img className="logo" src={logo} alt="logo" />
+                    <span className="text1">Japan Syria Friendship Association</span>
+                    <span className="text2">Non-Governmental Organization</span>
                 </div>
-                :
-                index===1
-                ?
-                <div class="slideshow-container">
-                    <div class="mySlides hide" />
-                    <div class="mySlides show image2">
-                        <span className="text1">Japan Syria Friendship Association</span>
-                        <span className="text2">Non-Governmental Organization</span>
-                    </div>
-                    <div class="mySlides hide" />
-                </div>
-                :
-                <div class="slideshow-container">
-                    <div class="mySlides hide" />
-                    <div class="mySlides hide fade" />
-                    <div class="mySlides show image3">
-                        <span className="text1">Japan Syria Friendship Association</span>
-                        <span className="text2">Non-Governmental Organization</span>
-                    </div>
-                </div>
-                }
+            </div>
         </div>
     )
 }
