be6a59c5f626d7648f78754df3939242f0c8fc2b
Nour Balaha <nourbalaha0@gmail.com>
2020-09-06 01:16:16 +0900

    created footer component

---
+++ src/components/Footer/Footer.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Footer.style.scss'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <span className="footer-text">© 2020 Japan Syria Friendship Association.</span>
+        </div>
+    )
+}
